9bdf9507852039f7a2db165d6aaae61185a0e8bb
Megi Fernanda <megifernand@gmail.com>
2017-10-25 18:49:27 +0700

    Profile Toolbar
    Add icons, Add Layout profile with toolbar icons.

--- app/src/main/java/com/mendev/airtelduplicate/MainActivity.java
+++ app/src/main/java/com/mendev/airtelduplicate/MainActivity.java
@@ -1,7 +1,10 @@
 package com.mendev.airtelduplicate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSubmit = (Button) findViewById(R.id.btn_Submit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
     }
 }
